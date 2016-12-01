78fdbb7eb0dbef10c61964d86c835f48852470a6
ZXJiang <rhwowo@outlook.com>
2016-12-01 18:41:07 +0800

    info.plist中定位开启选项的配置 地图功能的初步使用

--- JZXAMapTest/JZXAMapTest/ViewController.m
+++ JZXAMapTest/JZXAMapTest/ViewController.m
@@ -8,8 +8,9 @@
 
 #import "ViewController.h"
 #import <MAMapKit/MAMapKit.h>
-@interface ViewController ()
-
+@interface ViewController ()<MAMapViewDelegate>
+/** mapView */
+@property (strong, nonatomic) MAMapView *mapView;
 @end
 
 @implementation ViewController
@@ -17,9 +18,19 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    MAMapView *mapView = [[MAMapView alloc]initWithFrame:self.view.bounds];
     
-    [self.view addSubview:mapView];
+    [self setUpMapView];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    MAPointAnnotation *pointAnnotation = [[MAPointAnnotation alloc] init];
+    pointAnnotation.coordinate = CLLocationCoordinate2DMake(39.989631, 116.481018);
+    pointAnnotation.title = @"方恒国际";
+    pointAnnotation.subtitle = @"阜通东大街6号";
+    
+    [self.mapView addAnnotation:pointAnnotation];
+    
+//    [self.mapView setZoomLevel:10 animated:YES];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -27,14 +38,45 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark - SetUpMethod
+- (void)setUpMapView {
+    [self.view addSubview:self.mapView];
 }
-*/
+
+#pragma mark - Attribute 
+- (MAMapView *)mapView{
+    if (!_mapView) {
+        _mapView = [[MAMapView alloc] initWithFrame:self.view.bounds];
+//        _mapView.language = MAMapLanguageEn;
+        _mapView.showsScale = NO;
+//        _mapView.showTraffic = YES;
+        _mapView.showsUserLocation = YES;
+//        _mapView.showsCompass = NO;
+        _mapView.compassOrigin = CGPointMake(_mapView.center.x-_mapView.compassSize.width*0.5, _mapView.center.y-_mapView.compassSize.height*0.5);
+        _mapView.delegate = self;
+        
+    }
+    return _mapView;
+}
+
+#pragma mark - MAMapViewDelegate
+- (MAAnnotationView *)mapView:(MAMapView *)mapView viewForAnnotation:(id <MAAnnotation>)annotation
+{
+    if ([annotation isKindOfClass:[MAPointAnnotation class]])
+    {
+        static NSString *pointReuseIndentifier = @"pointReuseIndentifier";
+        MAPinAnnotationView*annotationView = (MAPinAnnotationView*)[mapView dequeueReusableAnnotationViewWithIdentifier:pointReuseIndentifier];
+        if (annotationView == nil)
+        {
+            annotationView = [[MAPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:pointReuseIndentifier];
+        }
+        annotationView.canShowCallout= YES;       //设置气泡可以弹出，默认为NO
+        annotationView.animatesDrop = YES;        //设置标注动画显示，默认为NO
+        annotationView.draggable = YES;        //设置标注可以拖动，默认为NO
+        annotationView.pinColor = MAPinAnnotationColorRed;
+        return annotationView;
+    }
+    return nil;
+}
 
 @end
